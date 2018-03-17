45503ee44056366cd4840998a98d53ee7347b982
parkstar <parkstar90@gmail.com>
2018-03-18 01:34:54 +0900

    리뷰 반영 - final등록

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Calculator calculator = new Calculator();
+        final Calculator calculator = new Calculator();
 
         int result = calculator.sum(3,5);
 
