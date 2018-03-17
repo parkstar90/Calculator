5371f1ea9b3f4ba4eb03898c9f04e86afe6abc8a
parkstar <parkstar90@gmail.com>
2018-03-18 01:11:07 +0900

    더하기 기능 구현

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        int result = calculator.sum(3,5);
+
+        System.out.println(result);
+    }
+}
