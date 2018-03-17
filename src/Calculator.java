44300ad18522cfe76267bd96ef68da6d982b6325
parkstar <parkstar90@gmail.com>
2018-03-18 01:35:27 +0900

    리뷰반영-javaDoc주석 등록

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,12 @@
 public class Calculator {
 
+    /**
+     * a + b 를 리턴한다.
+     *
+     * @param a
+     * @param b
+     * @return
+     */
     public int sum(int a, int b){
         return a + b;
     }
